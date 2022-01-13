75cb9e51d3903f8d350421e8f219b4ce2ae723fa
Jackson Doyle <jackconnerdoyle@gmail.com>
2022-01-12 21:37:17 -0800

    v2

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
+const generateImage = require("./generateImage")
+
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
 })
+
+
+client.on("messageCreate", (message) => {
+    if(message.content == "hi"){
+        message.reply("Hello World!")
+    }
+})
+
+const welcomeChannelId = "931047963818602516"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+  member.guild.channels.cache.get(welcomeChannelId).send({
+      content:`<@${member.id}> Welcome to the Valhalla!`,
+      files: [img]
+  }) 
+})
+
 
 client.login(process.env.TOKEN);
